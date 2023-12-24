ea3ea38db8950bf8f8eb903b613508efd9adb287
Glitch3086 <Glitch3086@gmail.com>
2023-12-23 20:42:52 -0600

    Added button funcntlity

---
+++ main.js
@@ -0,0 +1,17 @@
+function moveButton() {
+    const button = document.getElementById('noButton');
+    const maxX = window.innerWidth - button.clientWidth;
+    const maxY = window.innerHeight - button.clientHeight;
+
+    const newX = Math.random() * maxX;
+    const newY = Math.random() * maxY;
+
+    button.style.position = 'absolute';
+    button.style.left = `${newX}px`;
+    button.style.top = `${newY}px`;
+}
+
+function goToAnotherPage() {
+    window.location.href = "another-page.html";
+}
+
